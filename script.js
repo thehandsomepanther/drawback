a31b8a719b261524d18e25eb088b8e574179ae4a
Alex Cohen <alexcohen711@gmail.com>
2017-11-02 12:46:09 -0500

    Added erase mode and button to toggle (v 1.0.0)

--- script.js
+++ script.js
@@ -7,6 +7,12 @@ var canvas = document.querySelector('canvas');
 var ctx = canvas.getContext('2d');
 var lastX = 0, lastY = 0;
 
+function changeMode() {
+    if (status == ERASING_MODE) status = DRAWING_MODE
+    else if (status == DRAWING_MODE) status = ERASING_MODE
+    console.log(status)
+}
+
 function draw(ctx,x,y,size) {
     if (lastX && lastY && (x !== lastX || y !== lastY)) {
         ctx.fillStyle = "#000000";
@@ -25,10 +31,23 @@ function draw(ctx,x,y,size) {
     lastY = y;
 }
 
+function erase(ctx,x, y, size) {
+    ctx.clearRect(x, y, 30, 30); 
+}
 canvas.addEventListener('touchmove', function(e) {
     var point = e.targetTouches[0];
-    draw(ctx, point.clientX, point.clientY, 2);
-    window.navigator.vibrate(50);
+    console.log(status)
+    switch(status) {
+        case DRAWING_MODE:
+            draw(ctx, point.clientX, point.clientY, 2);
+            break;
+        case ERASING_MODE:
+            console.log('suh')
+            erase(ctx, point.clientX, point.clientY, 2);
+            break;
+        case EDITING_MODE:
+            window.navigator.vibrate(50);
+    }
 });
 
 canvas.addEventListener('touchend', function() {
